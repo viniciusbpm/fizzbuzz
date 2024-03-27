71e1fa1946f834989380bf112bbe14d7afcd5675
Vinícius <viniciusmpacheco22@gmail.com>
2024-03-27 09:54:18 -0300

    removing unused attribute

--- fizzbuzz-tdd/src/main/java/FizzBuzz.java
+++ fizzbuzz-tdd/src/main/java/FizzBuzz.java
@@ -1,5 +1,4 @@
 public class FizzBuzz {
-    private String output;
     public String getOutputFor(int input){
         FizzBuzzRule rule = new FizzBuzzRule(input);
         return rule.getOutput();
